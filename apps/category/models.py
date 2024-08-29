b76e07fd5b8944796f55ea34c6fc2e0c123aef6a
baielbekenov <baelbekbolotovb@gmail.com>
2024-08-29 12:53:43 +0600

    'dd'

--- apps/category/models.py
+++ apps/category/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=150, unique=True, null=False, verbose_name='Название')
+    image = models.ImageField(upload_to='images/category/%Y/%m', blank=True, null=True, verbose_name='Фото')
     created_at = models.DateField(auto_now_add=True, verbose_name='Дата создание')
 
     class Meta:
