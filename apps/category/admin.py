698acc5efcecb87c476085227f7559d19063f1dd
baielbekenov <baelbekbolotovb@gmail.com>
2024-10-12 13:32:06 +0600

    'dd'

--- apps/category/admin.py
+++ apps/category/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from apps.category.models import Category, AppBar
+from apps.category.models import Category, Banner
 
 admin.site.register(Category)
-admin.site.register(AppBar)
+admin.site.register(Banner)
