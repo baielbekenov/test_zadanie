a33cfaedebd162e2f7e4feedbbe50badfac97158
baielbekenov <baelbekbolotovb@gmail.com>
2024-10-12 14:22:31 +0600

    'ff'

--- apps/products/models.py
+++ apps/products/models.py
@@ -12,13 +12,24 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена', default=0)
     weight = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Вес (кг)', blank=True, null=True)
     created_at = models.DateField(auto_now_add=True, verbose_name='Дата создание')
+    ordering = models.PositiveIntegerField(verbose_name='Порядок', default=0)
 
     class Meta:
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
+        ordering = ['ordering']
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if self.ordering == 0:
+            last_product = Product.objects.order_by('-ordering').first()
+            if last_product:
+                self.ordering = last_product.ordering + 1
+            else:
+                self.ordering = 1
+        super().save(*args, **kwargs)
 
 
 class Image(models.Model):
