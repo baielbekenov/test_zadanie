21071ec941016a8ecd20b0d5eac4a7d9b8eb156a
baielbekenov <baelbekbolotovb@gmail.com>
2024-10-12 14:32:22 +0600

    'dd'

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 
 from apps.products.models import Product, Image, Cart, CartItems
 
-admin.site.register(Product)
+
+@admin.register(Product)
+class ProductcAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category_id', 'price', 'weight', 'ordering')
+
+
 admin.site.register(Image)
 admin.site.register(Cart)
 admin.site.register(CartItems)
